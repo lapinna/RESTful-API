1fd259f8e1df711c5bf95a48bbecfb351e5eb094
Inese Lapina <lapinna.inese@gmail.com>
2022-09-12 13:46:42 +0200

    add login

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import designPatternModel from "../models/designPatternModel.js"
+import designPatternModel from "../models/designPatternModel.js";
 
 export const createPattern = async (req, res) => {
   try {
@@ -13,6 +13,30 @@ export const createPattern = async (req, res) => {
     res.status(201).send("New pattern is created!");
   } catch (error) {
     res.status(405).send(error);
+    console.error(error);
+  }
+};
+
+export const loginPattern = async (req, res) => {
+  try {
+    const pattern = await designPatternModel.findOne({
+      patternName: req.body.patternName,
+    });
+    if (!pattern) {
+      return res.status(405).send("Wrong user or password!");
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      pattern.password
+    );
+
+    if (!isPasswordCorrect) {
+      return res.status(405).send("Wrong user or password!");
+    }
+
+    return res.status(201).send("Successfully LOGGED in!");
+  } catch (error) {
     console.error(error);
   }
 };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { createPattern } from "../controller/authController.js";
+import { createPattern, loginPattern } from "../controller/authController.js";
 
 const router = express.Router();
 
 router.post("/register", createPattern);
+
+router.post("/login", loginPattern);
 
 export default router;
